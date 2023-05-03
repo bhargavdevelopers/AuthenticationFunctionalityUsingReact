1c213cabd34fcfebe13fb493cd3d8da35a177ff2
Bhargav <gbhargav.engineer@gmail.com>
2023-05-03 07:56:14 +0000

    autentication

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="navbar">
+    <ul className="list">
+      <li>
+        <Link to="/" className="link">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" className="link">
+          About
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+export default Header
